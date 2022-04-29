7bbb43bdd99f62f8ca21d6d6c108a6bfd31dfd5b
Yoann-Renard <yoann.rnrd@gmail.com>
2022-04-29 10:39:10 +0200

    ValueError & KeyboardInterrupt

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 #### ╔╣█╠╗╚╝╔╣█╠╗ ####
 if __name__ == "__main__":
-    #CloseFirstTab()
-    user_or_group_to_spam = input("User or group to spam: ")
-    message_to_spam = input("Message to spam: ")
-    seconds_inter = int(input("Interval: "))
-    from src.src import *
-    fb = Facebook()
-    fb.connection()
-    fb.sendMessage(user_or_group_to_spam, message_to_spam, True)
-    print(f"Message: {message_to_spam} sent to {user_or_group_to_spam}.")
-    def spam():
-        driver.find_element_by_css_selector(
-            'div[aria-label="Écrire un message"]').send_keys(message_to_spam + '\n')
+    try:
+        #CloseFirstTab()
+        user_or_group_to_spam = input("User or group to spam: ")
+        message_to_spam = input("Message to spam: ")
+        seconds_inter = int(input("Interval: "))
+        from src.src import *
+        fb = Facebook()
+        fb.connection()
+        fb.sendMessage(user_or_group_to_spam, message_to_spam, True)
+        print(f"Spammer set with a {seconds_inter}s delay.")
         print(f"Message: {message_to_spam} sent to {user_or_group_to_spam}.")
-    print(f"Spammer set with a {seconds_inter}s delay.")
-    time.sleep(seconds_inter)
-    inter = setInterval(seconds_inter, spam)
+        def spam():
+            driver.find_element_by_css_selector(
+                'div[aria-label="Écrire un message"]').send_keys(message_to_spam + '\n')
+            print(f"Message: {message_to_spam} sent to {user_or_group_to_spam}.")
+        time.sleep(seconds_inter)
+        inter = setInterval(seconds_inter, spam)
+    except KeyboardInterrupt:
+        print("\nBye.")
+        exit(0)
+    except ValueError:
+        print("Interval must be a number.")
+        exit(1)
     
